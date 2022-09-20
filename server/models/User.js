922af97abe43459acb2cb7a669d41a415177e9c7
Sophie Cheng <chengsophie25@gmail.com>
2022-09-20 12:27:20 +0200

    update server

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const UserSchema  = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    profilePicture:{
+        type: String,
+        default: ""
+    },
+    followers:{
+        type: Array,
+        default: [],
+    },
+    followings:{
+        type: Array,
+        default: [],
+    },
+},
+    {timestamps: true}
+);
+
+// all users will have same user schema = same model
+module.exports = mongoose.model("User", UserSchema);
