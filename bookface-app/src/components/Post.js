808c327eb68be1fb94668ae1a4f904d397e7a0a7
Arthur Pluym <tuur.pluym@gmail.com>
2022-09-22 10:07:21 +0200

    updated branch dev

---
+++ bookface-app/src/components/Post.js
@@ -0,0 +1,58 @@
+import "../components/Post.css";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+// import  Users  from "../img/logo.png";
+// import  logo  from "../img/home1.jpg";
+import { Users } from "./Datas";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import { useState } from "react";
+
+// import Avatar from '@mui/material/Avatar';
+
+export default function Post({ post }) {
+  console.log(post);
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src=
+              {Users.filter((u) => u.id === post?.userId)[0].profilePicture}
+              alt=""
+            />
+           {/* <Avatar alt="Travis Howard" src={Users} /> */}
+
+            <span className="postUsername">
+              {Users.filter((u) => u.id === post?.userId)[0].username}
+            </span>
+            <span className="postDate">
+              {post.date}
+              </span>
+          </div>
+          <div className="postTopRight">
+            <MoreVertIcon />
+          </div>
+        </div>
+        <div className="postCenter">
+           <span className="postText">{post?.desc}</span> 
+           <img  className ='postImg' src={post.photo} alt="" /> 
+
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+         <span className="likeIcon">{<FavoriteIcon />}</span> 
+         <span className="likeIcon-comments" >{<SmsOutlinedIcon />}</span> 
+         <span className="likeIcon-share">{<SendOutlinedIcon />}</span>
+          <span className="postLikeCounter"> {post.like}people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment} comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
