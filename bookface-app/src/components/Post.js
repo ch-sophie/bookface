80035f10332eb72bb1378ba9f016bbabcc179166
Bhama <bhamaguruswami@gmail.com>
2022-09-22 11:24:52 +0200

    comment reloved

--- bookface-app/src/components/Post.js
+++ bookface-app/src/components/Post.js
@@ -1,30 +1,38 @@
 import "../components/Post.css";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 // import  Users  from "../img/logo.png";
-// import  logo  from "../img/home1.jpg";
+import  logo  from "./img/home2.jpg";
 import { Users } from "./Datas";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+// import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 import { useState } from "react";
 
 // import Avatar from '@mui/material/Avatar';
 
 export default function Post({ post }) {
-  console.log(post);
+  console.log(Users);
+  const [like,setLike] = useState(post.like)
+  const [isLiked,setIsLiked] = useState(false)
+
+  const likeButton =()=>{
+    setLike(isLiked ? like-1 : like+1)
+    setIsLiked(!isLiked)
+  }
+
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-          <img
+            
+            <img
             src={require('./img/' +
             Users.filter((u) => u.id === post.userId)[0].profilePicture +
             '.png')}
               className="postProfileImg"
               alt=""
             />
-           {/* <Avatar alt="Travis Howard" src={Users} /> */}
 
             <span className="postUsername">
               {Users.filter((u) => u.id === post?.userId)[0].username}
@@ -47,15 +55,21 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-         <span className="likeIcon">{<FavoriteIcon />}</span> 
-         <span className="likeIcon-comments" >{<SmsOutlinedIcon />}</span> 
-         <span className="likeIcon-share">{<SendOutlinedIcon />}</span>
-          <span className="postLikeCounter"> {post.like}people like it</span>
+         <span className="likeIcon" onClick={likeButton}>{<FavoriteIcon />}</span> 
+          <span className="likeIcon-comments" >{<SmsOutlinedIcon />}</span> 
+         {/* <span className="likeIcon-share">{<SendOutlinedIcon />}</span>  */}
+          <span className="postLikeCounter"> {like}people like it</span>
           </div>
-          <div className="postBottomRight">
-            <span className="postCommentText">{post.comment} comments</span>
-          </div>
+              {/* <span className="postCommentText">{post.comment} comments</span> */}
         </div>
+        <div className="postBottomRight ">
+
+        <div className="postCommentText">
+            <input type="text" placeholder="add comment"/>
+
+            </div>
+            </div>
+
       </div>
     </div>
   );
