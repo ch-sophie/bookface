53936404b29b24d76579c31a7321bdccd5fb14c5
Sophie Cheng <chengsophie25@gmail.com>
2022-10-04 15:19:21 +0200

    test comment

--- bookface-app/src/App.tsx
+++ bookface-app/src/App.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import SignUp from './components/signUp';
 import SignIn from './components/signIn';
-import Header from './components/header'
-import TestComponent from './components/testComponent'
+// import Header from './components/header'
+// import TestComponent from './components/testComponent'
 // import Editprofile from './components/Editprofile';
 import {
-  BrowserRouter,
+  // BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 import Hall from './components/hall'
 import Floor from './components/floor'
-import Test from './components/testComponent';
+// import Test from './components/testComponent';
 import ProfilePage from './components/Userprofile/ProfilePage';
 
 
