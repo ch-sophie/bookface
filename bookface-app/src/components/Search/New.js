818f3f5ec3de717c176fec6c84da731008099b65
Bhama <bhamaguruswami@gmail.com>
2022-10-04 10:24:49 +0200

    search page style

--- bookface-app/src/components/Search/New.js
+++ bookface-app/src/components/Search/New.js
@@ -8,6 +8,9 @@ import Box from '@mui/material/Box';
 import setimage from '../img/home1.jpg'
 import {Link} from 'react-router-dom'
 import { css, jsx } from '@emotion/react'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
 
 function New({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([]);
@@ -31,7 +34,16 @@ function New({ placeholder, data }) {
     setFilteredData([]);
     setWordEntered("");
   };
-
+  function srcset(image, size, rows = 1, cols = 1) {
+    return {
+      src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+      srcSet: `${image}?w=${size * cols}&h=${
+        size * rows
+      }&fit=crop&auto=format&dpr=2 2x`,
+    };
+  }
+  
+  
   return (
     // <Box sx={{ width: '100%', maxWidth: 500, }}>
 <div className="container-search" >
@@ -56,6 +68,7 @@ function New({ placeholder, data }) {
       </div>
       {filteredData.length != 0 && (
         <div className="dataResult">
+          
           {filteredData.slice(0, 15).map((value, key) => {
             return (
               <a className="dataItem" href={value.username} target="_blank">
@@ -73,7 +86,7 @@ function New({ placeholder, data }) {
         </div>
       )}
     </div>
-    <div className="gallery-search">
+    {/* <div className="gallery-search">
             <img  className='searchitem' src={setimage} alt="" />
             <img  className='searchitem' src={setimage} alt="" />
             <img  className='searchitem' src={setimage} alt="" />
@@ -86,12 +99,92 @@ function New({ placeholder, data }) {
             <img  className='searchitem' src={setimage} alt="" />
 
 
-           </div>
+           </div> */}
+           <ImageList
+      sx={{ width: 500, height: 450,margin:'auto'
+    }}
+      variant="quilted"
+      cols={4}
+      rowHeight={121}
+    >
+      {itemData.map((item) => (
+        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+          <img
+            // {...srcset(item.img, 121, item.rows, item.cols)}
+            src={require('../img/' +
+            item.img)}
+            alt={item.title}
+            loading="lazy"
+
+             /> 
+        </ImageListItem>
+      ))}
+    </ImageList>
 
            </div>
     // </Box>
 
   );
 }
+const itemData = [
+  {
+    img: 'book15.jpg',
+    title: 'Breakfast',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'book14.jpg',
+    title: 'Burger',
+  },
+  {
+    img: 'book13.jpg',
+    title: 'Camera',
+  },
+  {
+    img: 'book12.jpg',
+    title: 'Coffee',
+    cols: 2,
+  },
+  {
+    img: 'book11.jpg',
+    title: 'Hats',
+    cols: 2,
+  },
+  {
+    img: 'book16.jpg',
+    title: 'Honey',
+    author: '@arwinneil',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'book10.jpg',
+    title: 'Basketball',
+  },
+  {
+    img: 'book9.jpg',
+    title: 'Fern',
+  },
+  {
+    img: 'book8.jpg',
+    title: 'Mushrooms',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'book7.jpg',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'book6.jpg',
+    title: 'Sea star',
+  },
+  {
+    img: 'book5.jpg',
+    title: 'Bike',
+    cols: 2,
+  },
+];
 
 export default New;
